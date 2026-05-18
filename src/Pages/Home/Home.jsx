1a48fc2d52af1547e69c189227642aeb7709b318
Aniket Kumar <aniketpundir348@gmail.com>
2026-05-18 13:47:33 +0530

    Changes in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from "../../Components/Hero/Hero"
-import WelcomeText from "../../Components/WelcomeText/WelcomeText"
+// import WelcomeText from "../../Components/WelcomeText/WelcomeText"
 import WhyChooseUs from "../../Components/WhyChooseUs/WhyChooseUs"
 import BannerCTA from "../../Components/BannerCTA/BannerCTA"
 import Stats from "../../Components/Stats/Stats"
@@ -12,7 +12,7 @@ const Home = () => {
     return (
         <>
             <Hero />
-            <WelcomeText />
+            {/* <WelcomeText /> */}
             <ServiceCard/>
             <WhyChooseUs />
             <BannerCTA />
