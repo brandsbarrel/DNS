7a83c449b2ee8abd99bdbc72bcf913a59ff7a39b
abhinav-pratapsingh <abhinavpratap6142@gmail.com>
2026-05-20 15:21:06 +0530

    stats plus image and button of new image

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import Hero from "../../Components/Hero/Hero"
 // import WelcomeText from "../../Components/WelcomeText/WelcomeText"
 import WhyChooseUs from "../../Components/WhyChooseUs/WhyChooseUs"
-import BannerCTA from "../../Components/BannerCTA/BannerCTA"
-import Stats from "../../Components/Stats/Stats"
-import FindUs from "../../Components/FindUs/FindUs"
 import ServiceCard from '../../Components/ServiceCard/ServiceCard'
 import HowItWorks from '../../Components/HowItWorks/HowItWorks'
 
