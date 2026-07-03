6202a77d58c26dfefddd8c308b46f8c6f310dbb9
abhinav-pratapsingh <abhinavpratap6142@gmail.com>
2026-07-04 00:28:48 +0530

    fixed env

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,1 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentFilePath);
+const envPath = path.resolve(currentDir, "../../.env");
+
+dotenv.config({ path: envPath });
